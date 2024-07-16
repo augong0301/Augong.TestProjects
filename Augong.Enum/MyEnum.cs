b42bcc2fc6d19adf26639829a408582bbb474ba5
xgong <gongxy0301@163.com>
2024-07-16 17:37:20 +0800

    add enum test

---
+++ Augong.Enum/MyEnum.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Augong.Enum
+{
+	[Flags]
+	public enum SensorType
+	{
+		ScN = 1,// maped to AcqCard ch1; not change the sequence of SensorType
+		ScO = 2,
+		PL = 4,
+
+		Sp1 = 8,
+		ZrO2 = 16,
+		ZcO2 = 32,
+		Sp2 = 64,
+		None = 128, // maped to AcqCard ch8
+
+		Pho = 256, // Sp1 -Sp2
+
+		Sp12 = 512, //sp1 + sp2
+	}
+}
---
+++ Augong.Enum/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Augong.Enum;
+
+Console.WriteLine("Hello, World!");
+var sen = SensorType.ScN & SensorType.ScO;
+
+var dictionary = new Dictionary<SensorType,int>();
+dictionary.Add(sen,1);
+
+
+Console.WriteLine(dictionary[sen]);
+
+Console.ReadKey();
