117ce4ee646f4c48f035144e864e939b65af9373
xgong <gongxy0301@163.com>
2024-08-09 16:02:09 +0800

    add thread test

---
+++ Augong.ThreadTest/ThreadAbortTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Augong.ThreadTest
+{
+    public class ThreadAbortTest : ThreadTestBase
+    {
+        public override void DoTest()
+        {
+            var thread = new Thread(new ThreadStart(DoLoop));
+        }
+
+        private void DoLoop()
+        {
+            while (true) { var t = 1; };
+        }
+    }
+}
---
+++ Augong.ThreadTest/ThreadTestBase.cs
@@ -0,0 +1,12 @@
+﻿using Augong.CSharp.Contract;
+
+namespace Augong.ThreadTest
+{
+    public class ThreadTestBase : ITest
+    {
+        public virtual void DoTest()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
