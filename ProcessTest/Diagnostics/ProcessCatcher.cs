cee74e65febc83aa04e24a67695af923907e02d3
xgong <gongxy0301@163.com>
2024-08-09 10:18:40 +0800

    add processinsider

---
+++ ProcessTest/Diagnostics/ProcessCatcher.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Augong.ProcessTest.Diagnostics
+{
+    /// <summary>
+    /// Base class to manage process
+    /// </summary>
+    public class ProcessCatcher
+    {
+        internal Process _process;
+        public ProcessCatcher(string processName)
+        {
+            var processes = Process.GetProcessesByName(processName);
+
+            if (processes.Length <= 0)
+            {
+                
+            }
+        }
+    }
+}
---
+++ ProcessTest/Diagnostics/ProcessInsider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Augong.ProcessTest.Diagnostics
+{
+
+    /// <summary>
+    /// Aimed to look inside a process
+    /// </summary>
+    public class ProcessInsider(string processName)
+    {
+        public 
+    }
+}
