1a2e64e24126212e0f4f258ddf55c5aa5528bcae
xgong <gongxy0301@163.com>
2024-08-09 17:35:07 +0800

    add newPipe

---
+++ Augong.NewPipe/NewPipe.cs
@@ -0,0 +1,41 @@
+﻿using System.IO.Pipelines;
+
+namespace Augong.NewPipe
+{
+    public class NewPipe
+    {
+        private readonly Pipe _pipe;
+        public NewPipe(Pipe pipe)
+        {
+            _pipe = pipe;
+            _reader = new NewPipeReader(pipe);
+            _writer = new NewPipeWriter(pipe);
+        }
+
+        public NewPipeReader Reader => _reader;
+
+        private NewPipeReader _reader;
+
+        public NewPipeWriter Writer => _writer;
+
+        private NewPipeWriter _writer;
+
+
+        public void Reset()
+        {
+            try
+            {
+                _reader.Complete();
+                _writer.Complete();
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+                _pipe.Reset();
+            }
+        }
+    }
+}
