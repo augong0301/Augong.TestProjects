0616ebd94b5d6335b61ceab82e63b1db4219909d
翔宇 龚 <gongxy0301@163.com>
2024-08-18 14:26:54 +0800

    add str test

---
+++ Augong.StringTest/StringTester.cs
@@ -0,0 +1,12 @@
+﻿using Augong.CSharp.Contract;
+
+namespace Augong.StringTest
+{
+    public class StringTester : ITest
+    {
+        public void DoTest()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
