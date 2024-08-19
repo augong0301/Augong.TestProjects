12cce5929aa6153313078fec17034f0f28eb1c0b
xgong <gongxy0301@163.com>
2024-08-19 14:25:56 +0800

    string read test

--- Augong.StringTest/StringHelper.cs
+++ Augong.StringTest/StringHelper.cs
@@ -6,11 +6,22 @@ using System.Threading.Tasks;
 
 namespace Augong.StringTest
 {
-    public static class StringHelper
-    {
-        public static string[] GetStartFlag(string flag, string input)
-        {
-            return input.Split(flag);
-        }
-    }
+	public static class StringHelper
+	{
+		public static string[] GetStartFlag(string flag, string input)
+		{
+			return input.Split(flag);
+		}
+
+		public static List<string> ResortByLine(params string[] data)
+		{
+			List<string> rst = new List<string>();
+			foreach (var d in data)
+			{
+				var lines = d.Split("\r\n");
+				rst.AddRange(lines);
+			}
+			return rst;
+		}
+	}
 }
