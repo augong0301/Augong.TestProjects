e5412582bb4a7216b036ca38b99d2cdfc7211147
翔宇 龚 <gongxy0301@163.com>
2024-09-18 22:36:53 +0800

    add test

---
+++ Augong.Framework.Shared/SomeHelper.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Augong.Framework.Shared
+{
+    public class SomeHelper
+    {
+
+        public string DoSomething(object value)
+        {
+            var c = JsonConvert.SerializeObject(value);
+
+            return c;
+        }
+    }
+}
--- Augong.FrameworkConsole/Program.cs
+++ Augong.FrameworkConsole/Program.cs
@@ -1,77 +1,78 @@
-﻿using System;
+﻿using Augong.Framework.Shared;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace Augong.FrameworkConsole
 {
-	internal class Program
-	{
-		static void Main(string[] args)
-		{
-			var reply = Console.ReadLine();
-			while (reply != string.Empty)
-			{
-				int startIndex = reply.IndexOf("#");
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = new SomeHelper();
+            var rst = s.DoSomething("ccccc");
+            var c= JsonConvert.SerializeObject(rst);
 
+            var assembly = Assembly.GetExecutingAssembly();
+            var referencedAssemblies = assembly.GetReferencedAssemblies();
 
-				var data = reply.Substring(startIndex + "#".Length + "1".Length);
+            foreach (var referencedAssembly in referencedAssemblies)
+            {
+                Console.WriteLine($"\tReferenced Assembly: {referencedAssembly.Name}, Version: {referencedAssembly.Version}");
+            }
+        }
+        private static void DoReadOnlyServerTest()
+        {
+            var cl = new ReadOnlyClient();
+            string input = string.Empty;
+            Console.WriteLine("ip =");
+            var ip = "192.168.42.100";
+            //var ip = "127.0.0.1";
+            Console.WriteLine("port =");
+            var port = 23;
+            var suc = cl.Connect(ip, port);
+            Console.WriteLine($"Connected {ip} : {port} is {suc}");
+            Console.WriteLine("Enter your commands");
+            while (input != null)
+            {
+                input = Console.ReadLine();
+                TryReceive(cl, input);
+            }
 
-				var c = data.Substring( "WAS".Length);
+            Console.ReadKey();
+        }
 
-			}
+        public static void TryReceive(ReadOnlyClient cl, string input)
+        {
+            cl.DoSend(input);
+            Console.WriteLine($"Send = {input}");
+            var cts = new CancellationTokenSource();
+            cl.TryReceive(cts);
 
-			DoReadOnlyServerTest();
-		}
-		private static void DoReadOnlyServerTest()
-		{
-			var cl = new ReadOnlyClient();
-			string input = string.Empty;
-			Console.WriteLine("ip =");
-			var ip = "192.168.42.100";
-			//var ip = "127.0.0.1";
-			Console.WriteLine("port =");
-			var port = 23;
-			var suc = cl.Connect(ip, port);
-			Console.WriteLine($"Connected {ip} : {port} is {suc}");
-			Console.WriteLine("Enter your commands");
-			while (input != null)
-			{
-				input = Console.ReadLine();
-				TryReceive(cl, input);
-			}
+        }
 
-			Console.ReadKey();
-		}
+        public static void DoReceive(ReadOnlyClient cl, string input)
+        {
+            cl.DoSend(input);
 
-		public static void TryReceive(ReadOnlyClient cl, string input)
-		{
-			cl.DoSend(input);
-			Console.WriteLine($"Send = {input}");
-			var cts = new CancellationTokenSource();
-			cl.TryReceive(cts);
-
-		}
-
-		public static void DoReceive(ReadOnlyClient cl, string input)
-		{
-			cl.DoSend(input);
-
-			bool end = false;
-			int count = 0;
-			var lines = new List<string>();
-			var rtn = cl.DoReceive();
-			count++;
-			end = rtn.Contains("MFOCUS");
-			Console.WriteLine("Received = " + rtn);
-			//lines.AddRange(l);
-			foreach (var l in lines)
-			{
-				Console.WriteLine(l);
-			}
-			Console.WriteLine($"Receive data done by{count}");
-		}
-	}
+            bool end = false;
+            int count = 0;
+            var lines = new List<string>();
+            var rtn = cl.DoReceive();
+            count++;
+            end = rtn.Contains("MFOCUS");
+            Console.WriteLine("Received = " + rtn);
+            //lines.AddRange(l);
+            foreach (var l in lines)
+            {
+                Console.WriteLine(l);
+            }
+            Console.WriteLine($"Receive data done by{count}");
+        }
+    }
 }
